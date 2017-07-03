352640ab29819dfef1e5364777415c2fde034fd7
eloyucu <rockacolla@hotmail.com>
2017-07-03 16:28:01 +0200

    Fix test

--- test/xml_to_keyword_test.exs
+++ test/xml_to_keyword_test.exs
@@ -4,27 +4,31 @@ defmodule XmlToKeywordTest do
 
   test "Convert an easy structure" do
     xml = XmlToKeyword.convert("<foo>bar</foo>")
-    assert xml == {:ok, {:foo, [:foo, "bar"]}}
+    assert xml == {:ok, {:foo, [:foo, %{}, "bar"]}}
   end
   test "Convert a structure with attributes" do
     xml = XmlToKeyword.convert("<body><foo at='value' from='re-value'>bar</foo></body>")
-    assert xml == {:ok, {:body, [:body, [{:foo, %{at: "value", from: "re-value"}, "bar"}]]}}
+    assert xml == {:ok, {:body, [:body, %{}, [{:foo, %{at: "value", from: "re-value"}, "bar"}]]}}
   end
   test "Convert a structure a little more complex without attributes" do
     xml = XmlToKeyword.convert("<body><foo><do><redo>bar</redo></do></foo></body>")
-    assert xml == {:ok, {:body, [:body, [foo: [do: [redo: "bar"]]]]}}
+    assert xml == {:ok, {:body, [:body, %{}, [foo: [do: [redo: "bar"]]]]}}
+  end
+  test "Convert a structure a little more complex without attributes on parent node" do
+    xml = XmlToKeyword.convert("<body attr='value'><foo><do><redo>bar</redo></do></foo></body>")
+    assert xml == {:ok, {:body, [:body, %{attr: "value"}, [foo: [do: [redo: "bar"]]]]}}
   end
   test "Convert a structure a little more complex with attributes" do
     xml = XmlToKeyword.convert("<body><foo at='value' from='re-value'><do><redo at='value2'>bar</redo></do></foo></body>")
-    assert xml == {:ok, {:body, [:body, [{:foo, %{at: "value", from: "re-value"}, [do: [{:redo, %{at: "value2"}, "bar"}]]}]]}}
+    assert xml == {:ok, {:body, [:body, %{}, [{:foo, %{at: "value", from: "re-value"}, [do: [{:redo, %{at: "value2"}, "bar"}]]}]]}}
   end
   test "Convert a structure with the xml header" do
     xml = XmlToKeyword.convert("<?xml version='1.0' encoding='UTF-8'?><body><foo at='value' from='re-value'><do><redo at='value2'>bar</redo></do></foo></body>")
-    assert xml == {:ok, {:body, [:body, [{:foo, %{at: "value", from: "re-value"}, [do: [{:redo, %{at: "value2"}, "bar"}]]}]]}}
+    assert xml == {:ok, {:body, [:body, %{}, [{:foo, %{at: "value", from: "re-value"}, [do: [{:redo, %{at: "value2"}, "bar"}]]}]]}}
   end
   test "Convert from file" do
     {result, content} = File.read "./test/templates/template.xml"
     xml = XmlToKeyword.convert(content)
-    assert xml == {:ok, {:body, [:body, [{:foo, %{at: "value", from: "re-value"}, "\n    bar\n  "}]]}}
+    assert xml == {:ok, {:body, [:body, %{}, [{:foo, %{at: "value", from: "re-value"}, "\n    bar\n  "}]]}}
   end
 end
